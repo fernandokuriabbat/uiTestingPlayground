2017cb9ff9d32b25eb29af4de63bd87a02305dcf
FrnndKuri <fernandokuriabbat@gmail.com>
2025-10-29 00:18:52 -0500

    chore(scaffold): agrega skeleton de POM y specs para UITAP

---
+++ page-objects/basePage.ts
@@ -0,0 +1,14 @@
+import { Page, expect, Locator } from "@playwright/test";
+
+export abstract class BasePage{
+    protected readonly page: Page;
+
+    constructor(page: Page){
+        this.page = page;   
+    }
+
+    async scrollIntoView(locator: Locator){
+        await locator.scrollIntoViewIfNeeded();
+        await expect(locator).toBeVisible(); 
+    }
+}
