da454f167d0cf8cfc92cf06293153b4446e99d60
FrnndKuri <fernandokuriabbat@gmail.com>
2025-11-09 21:58:25 -0500

    .

--- page-objects/mouseOverPage.ts
+++ page-objects/mouseOverPage.ts
@@ -3,8 +3,19 @@ import { BasePage } from "./basePage";
 
 export class MouseOverPage extends BasePage{
 
+    private readonly clickMeLink: Locator; 
+    private readonly linkButtonLink: Locator; 
+
     constructor(page: Page){
         super(page);
+        this.clickMeLink = this.page.getByRole('link', {name: 'Click me'});
+        this.linkButtonLink = this.page.getByRole('link', {name: 'Link Button'});
+    }
+
+    async clickTwoTimesOnClickMeLick(n = 2){
+        for(let i = 0; i < 2; i++) {
+            await this.clickMeLink.click(); 
+        }
     }
 
 
