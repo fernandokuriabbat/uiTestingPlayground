8990c9403218f6f48506701d9804f8efe6fce3ea
FrnndKuri <fernandokuriabbat@gmail.com>
2025-11-18 21:18:11 -0500

    feat: added pruebaPrueba.ts

---
+++ page-objects/pruebaPrueba.ts
@@ -0,0 +1,13 @@
+import { Page, expect, Locator } from "@playwright/test";
+import { BasePage } from "./basePage";
+
+export class PruebaPrueba extends BasePage{
+
+    constructor(page: Page){
+        super(page);
+     
+    }
+   
+
+
+}
